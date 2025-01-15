e583de35455ccbbd85da439135617d83f8c2f5f8
mawenzhen <mawenzhen@yaeher.com>
2025-01-15 17:08:47 +0800

    feat: stack

---
+++ stack_and_tree/stack.java
@@ -0,0 +1,33 @@
+package stack_and_tree;
+
+import java.util.Stack;
+
+public class stack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(3);
+        stack.push(2);
+        stack.push(5);
+        stack.push(4);
+
+        System.out.println("栈 stack = " + stack);
+
+        /* 访问栈顶元素 */
+        int peek = stack.peek();
+        System.out.println("栈顶元素 peek = " + peek);
+
+        /* 元素出栈 */
+        int pop = stack.pop();
+        System.out.println("出栈元素 pop = " + pop + "，出栈后 stack = " + stack);
+
+        /* 获取栈的长度 */
+        int size = stack.size();
+        System.out.println("栈的长度 size = " + size);
+
+        /* 判断是否为空 */
+        boolean isEmpty = stack.isEmpty();
+        System.out.println("栈是否为空 = " + isEmpty);
+    }
+}
