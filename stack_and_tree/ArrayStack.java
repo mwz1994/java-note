1d84023889a4cec536c3313d791f6fe793356d37
yaeher/mawenzhen <mawenzhen@yaeher.com>
2025-01-19 21:12:38 +0800

    feat(stack_listnode): 链表表达stack

---
+++ stack_and_tree/ArrayStack.java
@@ -0,0 +1,38 @@
+package stack_and_tree;
+
+import java.util.ArrayList;
+
+public class ArrayStack {
+    private ArrayList<Integer> stack;
+
+    ArrayStack(){
+        stack = new ArrayList<>();
+    }
+
+
+    /* 获取栈的长度 */
+    public int size(){
+
+    }
+
+    /* 入栈 */
+    public void push(int num){
+
+    }
+
+    /* 出栈 */
+    public int pop() {
+
+    }
+
+    /* 访问栈顶元素 */
+    public int peek(){
+
+    }
+
+    /* 将 List 转化为 Array 并返回 */
+    public Object[] toArray(){
+
+    }
+
+}
