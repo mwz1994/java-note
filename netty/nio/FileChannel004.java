b94a37a34efd0fe4c0605246f5b39d1187719ecb
mawenzhen <mawenzhen@yaeher.com>
2025-04-03 10:34:56 +0800

    FileChannel read direct

---
+++ netty/nio/FileChannel004.java
@@ -0,0 +1,25 @@
+package netty.nio;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class FileChannel004 {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("/Users/wenzhenma/Desktop/file-output.txt");
+
+        FileChannel fileChannel = FileChannel.open(file.toPath(), StandardOpenOption.READ);
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate((int) file.length());
+
+        fileChannel.read(byteBuffer);
+
+        String result = new String(byteBuffer.array());
+
+        System.out.println("read result is --> "+ result);
+    }
+}
