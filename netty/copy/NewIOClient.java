653ba0837e06ad24401c4bb9eaac3a29534b1e18
mawenzhen <mawenzhen@yaeher.com>
2025-04-09 14:52:27 +0800

    transfer demo

---
+++ netty/copy/NewIOClient.java
@@ -0,0 +1,26 @@
+package netty.copy;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+
+public class NewIOClient {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("localhost",7001));
+
+        String filename = "/Users/wenzhenma/Desktop/《凤凰架构》.pdf";
+
+        FileChannel fileChannel = new FileInputStream(filename).getChannel();
+
+        long startTime = System.currentTimeMillis();
+
+        long transferCount = fileChannel.transferTo(0, fileChannel.size(), socketChannel);
+
+        System.out.println("发送的总的字节数 = "+ transferCount + " 耗时: " + (System.currentTimeMillis() - startTime));
+
+        fileChannel.close();
+    }
+}
---
+++ netty/copy/NewIOServer.java
@@ -0,0 +1,38 @@
+package netty.copy;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class NewIOServer {
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress address = new InetSocketAddress(7001);
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+
+        ServerSocket serverSocket = serverSocketChannel.socket();
+
+        serverSocket.bind(address);
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+
+        while (true){
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            int readCount = 0;
+
+            while (-1 != readCount){
+                try {
+                    readCount = socketChannel.read(byteBuffer);
+                    String str = new String(byteBuffer.array());
+                    System.out.println("receive string is --> ["+ str +"]");
+                }catch (Exception e){
+                    break;
+                }
+                //
+                byteBuffer.rewind();
+            }
+        }
+    }
+}
